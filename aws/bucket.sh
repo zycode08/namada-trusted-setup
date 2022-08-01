70c5967574a64beb2627eada0933f6606068a4f6
Fraccaroli Gianmarco <Fraccaman@users.noreply.github.com>
2022-08-01 13:00:26 +0200

    [ci] fix build docker (#46)
    Co-authored-by: Gianmarco Fraccaroli <>

--- aws/bucket.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -x
-awslocal s3 mb s3://bucket
-set +x
--- docker/compile.sh
+++
@@ -1,1 +0,0 @@
-RUSTFLAGS='-Clinker=rust-lld' cargo build --release --features=parallel --bin phase1-coordinator --target x86_64-unknown-linux-musl
