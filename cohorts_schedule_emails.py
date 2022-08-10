6c70eac91ecd225b04291c57b2d44310958a92eb
gnosed <hiwz2ster@gmail.com>
2022-08-10 18:39:33 +0200

    api: update all merge tags from list, schedule campaigns

--- cohorts_schedule_emails.py
+++ cohorts_schedule_emails.py
@@ -10,64 +10,101 @@ from dotenv import load_dotenv
 import datetime
 
 load_dotenv()
+# Mailchimp API config
 MAILCHIMP_API_KEY = os.getenv('MAILCHIMP_API_KEY')
 MAILCHIMP_SERVER_PREFIX = os.getenv('MAILCHIMP_SERVER_PREFIX')
+# Ceremony Parameters
 MAILCHIMP_TS_LIST_ID = os.getenv('MAILCHIMP_TS_LIST_ID')
 CEREMONY_START_DATE = os.getenv('CEREMONY_START_DATE')
 
+ceremony_start_date = datetime.datetime.strptime(
+    CEREMONY_START_DATE, "%Y-%m-%d %H:%M:%S")
+
+campaign_settings = {
+    "subject_line": "subject_line",
+    "preview_text": "preview_text",
+    "title": "title",
+    "from_name": "from_name",
+    "reply_to": "newsletter@anoma.network",
+    "template_id": 10276251
+}
+# FIXME: testing purposes only, remove me
 email = "hiwz2ster@gmail.com"
 token = "test_token"
 emails = ["hiwz2ster@gmail.com"]
 cohort = 1
-cohort_tag = "ts_cohort_" + str(cohort)
-template_id = 10276251
+number_of_cohorts = 5
 
-start_date = datetime.datetime.strptime(
-    CEREMONY_START_DATE, "%Y-%m-%d %H:%M:%S")
-cohort_datetime = start_date + datetime.timedelta(days=cohort)
-cohort_date_str = cohort_datetime.strftime("%Y-%m-%d")
 
-print(start_date)
-print(cohort_datetime)
+def cohort_tag(cohort): return "ts_cohort_" + str(cohort)
 
-try:
-    client = MailchimpMarketing.Client()
-    client.set_config({
-        "api_key": MAILCHIMP_API_KEY,
-        "server": MAILCHIMP_SERVER_PREFIX
-    })
-# Update merge tags/fields with the latest ceremony_token and cohort_start_date
-    response_set_list_member = client.lists.set_list_member(MAILCHIMP_TS_LIST_ID, email, {
-        "email_address": email, "status_if_new": "unsubscribed", "merge_fields": {"TSTOKEN": token, "TSCOHORT": cohort_date_str}
-    })
-    # response = client.lists.update_list_member_tags(
-    #     MAILCHIMP_TS_LIST_ID, email, {"tags": [{"name": cohort_tag, "status": "active"}]})
-    # Add cohort list to specific cohort-based segment
-    response_create_segment = client.lists.create_segment(
-        MAILCHIMP_TS_LIST_ID, {"name": cohort_tag, "static_segment": emails})
-    print(response_create_segment)
-    # Create campaign using existing template_id and send to specific cohort
-    response_create_campaign = client.campaigns.create(
-        {"type": "regular",
-         "recipients": {
-             "segment_opts": {
-                 "saved_segment_id": response_create_segment['id']
+
+def calculate_cohort_datetime(ceremony_start_date, cohort):
+    return ceremony_start_date + datetime.timedelta(days=cohort)
+
+# Update member list's merge tags: TSTOKEN, TSCOHORT
+
+
+def update_member_merge_tags(email, token, cohort_datetime):
+    try:
+        client = MailchimpMarketing.Client()
+        client.set_config({
+            "api_key": MAILCHIMP_API_KEY,
+            "server": MAILCHIMP_SERVER_PREFIX
+        })
+        cohort_date_str = cohort_datetime.strftime("%Y-%m-%d")
+        response = client.lists.set_list_member(MAILCHIMP_TS_LIST_ID, email, {
+            "email_address": email, "status_if_new": "unsubscribed", "merge_fields": {"TSTOKEN": token, "TSCOHORT": cohort_date_str}
+        })
+        print(response)
+
+    except ApiClientError as error:
+        print("Error: {}".format(error.text))
+
+
+def create_and_schedule_campaign(campaign_settings, emails, cohort_tag, cohort_datetime):
+    try:
+        client = MailchimpMarketing.Client()
+        client.set_config({
+            "api_key": MAILCHIMP_API_KEY,
+            "server": MAILCHIMP_SERVER_PREFIX
+        })
+        # Create segment from cohort email list
+        response_create_segment = client.lists.create_segment(
+            MAILCHIMP_TS_LIST_ID, {"name": cohort_tag, "static_segment": emails})
+        # Create campaign from template_id
+        response_create_campaign = client.campaigns.create(
+            {"type": "regular",
+             "recipients": {
+                 "segment_opts": {
+                     "saved_segment_id": response_create_segment['id']
+                 },
+                 "list_id": MAILCHIMP_TS_LIST_ID
              },
-             "list_id": MAILCHIMP_TS_LIST_ID
-         },
-            "settings": {
-             "subject_line": "subject_line",
-             "preview_text": "preview_text",
-             "title": "title",
-             "from_name": "from_name",
-             "reply_to": "newsletter@anoma.network",
-             "template_id": template_id
-         }})
-    print(response_create_campaign)
-    # Schedule previous campaign with corresponding datetime 
-    response_schedule_campaign = client.campaigns.schedule(response_create_campaign['id'], {
-        "schedule_time": cohort_datetime.isoformat()})
-    print(response_schedule_campaign)
+                "settings": campaign_settings
+             })
+        print(response_create_campaign)
+        # Schedule previous campaign with corresponding datetime
+        response_schedule_campaign = client.campaigns.schedule(response_create_campaign['id'], {
+            "schedule_time": cohort_datetime.isoformat()})
+        print(response_schedule_campaign)
 
-except ApiClientError as error:
-    print("Error: {}".format(error.text))
+    except ApiClientError as error:
+        print("Error: {}".format(error.text))
+
+
+for cohort in range(number_of_cohorts):
+    with open("tokens_test/namada_cohort_{}.json".format(cohort)) as f:
+        cohort_file = json.load(f)
+        cohort_datetime = calculate_cohort_datetime(
+            ceremony_start_date, cohort)
+        emails = []
+        print(cohort_tag(cohort))
+        for i in range(len(cohort_file)):
+            email = cohort_file[i][0]
+            emails.append(email)
+            token = cohort_file[i][1]
+            # update_member_merge_tags(email, token, cohort_datetime)
+            print("update_member_merge_tags: ", email, token, cohort_datetime)
+        print("create_and_schedule_campaign: ", campaign_settings, emails,
+              cohort_tag(cohort), cohort_datetime)
--- cohorts_tokens_generator.py
+++ cohorts_tokens_generator.py
@@ -9,7 +9,7 @@ load_dotenv()
 MUCH_RANDOM_SECRET = os.getenv('MUCH_RANDOM_SECRET')
 EMAILS_LIST_PATH = os.getenv('EMAILS_LIST_PATH')
 
-PARTICIPANTS_PER_COHORT = 1000
+PARTICIPANTS_PER_COHORT = os.getenv('PARTICIPANTS_PER_COHORT')
 
 emails = []
 tokens = []
@@ -19,22 +19,26 @@ secret = MUCH_RANDOM_SECRET
 with open(EMAILS_LIST_PATH, newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        emails.append(row[0])
+        email = row[0]
+        emails.append(email)
         # Generate unique token by hashing the email and a random secret: hash(email||secret)
         h = blake2b(digest_size=10)
-        h.update(bytes(row[0], 'utf-8') + bytes(secret, 'utf-8'))
+        h.update(bytes(email, 'utf-8') + bytes(secret, 'utf-8'))
         tokens.append(h.hexdigest())
 
+# Create a list of tuple [email, token]
 zipped_emails_tokens = list(map(list, zip(emails, tokens)))
 
 number_of_cohorts = math.ceil(len(emails) / PARTICIPANTS_PER_COHORT)
 
 for cohort in range(number_of_cohorts):
+    # Generate json file containing all tokens for a cohort
     with open("tokens_test/namada_tokens_cohort_{}.json".format(cohort), "w") as f:
         start = cohort * PARTICIPANTS_PER_COHORT
         end = (cohort + 1) * PARTICIPANTS_PER_COHORT
         f.write(json.dumps(tokens[start:end]))
 
+    # Generate json file containing the list of tuples [email, token] for a cohort
     with open("tokens_test/namada_cohort_{}.json".format(cohort), "w") as f:
         start = cohort * PARTICIPANTS_PER_COHORT
         end = (cohort + 1) * PARTICIPANTS_PER_COHORT
