50c444357e8c27534a1d2cd2ad8c798deb849233
Marco Granelli <marco.granelli@ymail.com>
2022-10-28 16:43:30 +0200

    Updates ASCII art

--- phase1-cli/src/ascii_logo.rs
+++ phase1-cli/src/ascii_logo.rs
@@ -1,26 +1,23 @@
-// Trivial file to hold the ascii logo for the CLI. Simplifies the prebuilt binaries generation.
+// Trivial file to hold the ascii logo for the CLI. Simplifies the pre-built binaries generation.
 
 #[rustfmt::skip]
-pub const ASCII_LOGO: &str = "
-ooooo      ooo       .o.       ooo        ooooo       .o.       oooooooooo.         .o.                                           
-`888b.     `8'      .888.      `88.       .888'      .888.      `888'   `Y8b       .888.                                          
- 8 `88b.    8      .8\"888.      888b     d'888      .8\"888.      888      888     .8\"888.                                         
- 8   `88b.  8     .8' `888.     8 Y88. .P  888     .8' `888.     888      888    .8' `888.                                        
- 8     `88b.8    .88ooo8888.    8  `888'   888    .88ooo8888.    888      888   .88ooo8888.                                       
- 8       `888   .8'     `888.   8    Y     888   .8'     `888.   888     d88'  .8'     `888.                                      
-o8o        `8  o88o     o8888o o8o        o888o o88o     o8888o o888bood8P'   o88o     o8888o                                     
-                                                                                                                                  
-                                                                                                                                  
-                                                                                                                                  
-ooooooooooooo                                   .                   .o8       .oooooo..o               .                          
-8'   888   `8                                 .o8                  \"888      d8P'    `Y8             .o8                          
-     888      oooo d8b oooo  oooo   .oooo.o .o888oo  .ooooo.   .oooo888      Y88bo.       .ooooo.  .o888oo oooo  oooo  oo.ooooo.  
-     888      `888\"\"8P `888  `888  d88(  \"8   888   d88' `88b d88' `888       `\"Y8888o.  d88' `88b   888   `888  `888   888' `88b 
-     888       888      888   888  `\"Y88b.    888   888ooo888 888   888           `\"Y88b 888ooo888   888    888   888   888   888 
-     888       888      888   888  o.  )88b   888 . 888    .o 888   888      oo     .d8P 888    .o   888 .  888   888   888   888 
-    o888o     d888b     `V88V\"V8P' 8\"\"888P'   \"888\" `Y8bod8P' `Y8bod88P\"     8\"\"88888P'  `Y8bod8P'   \"888\"  `V88V\"V8P'  888bod8P' 
-                                                                                                                        888       
-                                                                                                                       o888o
+pub const ASCII_LOGO: &str = r"
+ _   _                           _       
+| \ | | __ _ _ __ ___   __ _  __| | __ _ 
+|  \| |/ _` | '_ ` _ \ / _` |/ _` |/ _` |
+| |\  | (_| | | | | | | (_| | (_| | (_| |
+|_| \_|\__,_|_| |_| |_|\__,_|\__,_|\__,_|
+ _____               _           _       
+|_   _| __ _   _ ___| |_ ___  __| |      
+  | || '__| | | / __| __/ _ \/ _` |      
+  | || |  | |_| \__ \ ||  __/ (_| |      
+  |_||_|   \__,_|___/\__\___|\__,_|      
+ ____       _                            
+/ ___|  ___| |_ _   _ _ __               
+\___ \ / _ \ __| | | | '_ \              
+ ___) |  __/ |_| |_| | |_) |             
+|____/ \___|\__|\__,_| .__/              
+                     |_|  
 ";
 
 #[rustfmt::skip]
