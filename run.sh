b9c2983b2416c29a688f37ee1e6739db50e72e62
zycode08 <zycode08@gmail.com>
2022-11-28 03:30:34 +0000

    automate script

--- run.sh
+++ run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-wget -O Namada.json https://www.reddit.com/r/Namada.json
-tokens=$(cat Namada.json | jq '.data.children | .[].data | select(.title | test("Cohort 9")) | .selftext' | tr ' ' '\n' | grep -o "^9nFe[^\\\\|^\"]*")
+while :
+do
+  wget -O Namada.json https://www.reddit.com/r/Namada.json
+  tokens=$(cat Namada.json | jq '.data.children | .[].data | select(.title | test("Cohort 9")) | .selftext' | tr ' ' '\n' | grep -o "^9nFe[^\\\\|^\"]*")
 
-COUNTER=1
-for token in  $tokens;do
-  echo "token $COUNTER: $token"
-  nohup namada-ts contribute default https://contribute.namada.net $token &> log_${COUNTER}.txt &
-  COUNTER=$[$COUNTER +1]
+  if [[ -z "$tokens" ]]; then
+    echo "empty"
+    sleep 60
+    continue
+  else
+    COUNTER=1
+    for token in $tokens;do
+      echo "token $COUNTER: $token"
+      nohup namada-ts contribute default https://contribute.namada.net $token &> log_${COUNTER}.txt &
+      COUNTER=$[$COUNTER +1]
+    done
+
+    break
+  fi
 done
